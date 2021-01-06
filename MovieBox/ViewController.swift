a08e2db33818c7ed7e4732e970005ac90e7ba10f
Dinesh Tanwar <dtanwar@e-emphasys.com>
2021-01-06 19:23:30 +0530

    Initial Commit

---
+++ MovieBox/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieBox
+//
+//  Created by Dinesh Tanwar on 06/01/21.
+//  Copyright © 2021 Dinesh Tanwar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
