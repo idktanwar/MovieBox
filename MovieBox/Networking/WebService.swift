7a504439164f846cbd657bde9c4c516a2dd19594
Dinesh Tanwar <dtanwar@e-emphasys.com>
2021-01-06 23:19:43 +0530

    Dummy UI Screen for MovieList created

---
+++ MovieBox/Networking/WebService.swift
@@ -0,0 +1,10 @@
+//
+//  WebService.swift
+//  MovieBox
+//
+//  Created by Dinesh Tanwar on 06/01/21.
+//  Copyright © 2021 Dinesh Tanwar. All rights reserved.
+//
+
+import Foundation
+
---
+++ MovieBox/Utils/Extensions.swift
@@ -0,0 +1,17 @@
+//
+//  Extensions.swift
+//  MovieBox
+//
+//  Created by Dinesh Tanwar on 06/01/21.
+//  Copyright © 2021 Dinesh Tanwar. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: 1)
+    }
+    
+    static let lightBlue1 = UIColor.rgb(red: 29, green: 161, blue: 242)
+}
